e6b361dc3e77283124450f679879e03b465e85c5
David <liyuansheng@liyuanshengdeMac-mini.local>
2016-10-09 14:53:59 +0800

    1、优化SDK 2、fixed bug.

--- Unity3DForSMSSDK/Assets/Plugins/iOS/SMSSDK_Unity3dBridge.m
+++ Unity3DForSMSSDK/Assets/Plugins/iOS/SMSSDK_Unity3dBridge.m
@@ -61,59 +61,59 @@ extern "C" {
         [SMSSDK registerApp:appKeyStr withSecret:appSecretStr];
     }
     
-        void __iosGetVerificationCode (SMSGetCodeMethod smsGetCodeMethod, void *phoneNumber, void *zone,void *observer)
+    void __iosGetVerificationCode (SMSGetCodeMethod smsGetCodeMethod, void *phoneNumber, void *zone,void *observer)
+    {
+        NSString  *phoneNumberStr = [NSString stringWithCString:phoneNumber encoding:NSUTF8StringEncoding];
+        
+        NSString *zoneStr = [NSString stringWithCString:zone encoding:NSUTF8StringEncoding];
+        
+        NSString *observerStr  = [NSString stringWithCString:observer encoding:NSUTF8StringEncoding];
+        
+        if (phoneNumber && zone)
         {
-            NSString  *phoneNumberStr = [NSString stringWithCString:phoneNumber encoding:NSUTF8StringEncoding];
-            
-            NSString *zoneStr = [NSString stringWithCString:zone encoding:NSUTF8StringEncoding];
-            
-            NSString *observerStr  = [NSString stringWithCString:observer encoding:NSUTF8StringEncoding];
-            
-            if (phoneNumber && zone)
-            {
-                [SMSSDK getVerificationCodeByMethod:smsGetCodeMethod phoneNumber:phoneNumberStr zone:zoneStr customIdentifier:nil result:^(NSError *error) {
-                    
-                    NSMutableDictionary *resultDic = [NSMutableDictionary dictionaryWithCapacity:0];
-                    [resultDic setObject:[NSNumber numberWithInt:1] forKey:@"action"];
-                    
-                    if (!error )
+            [SMSSDK getVerificationCodeByMethod:smsGetCodeMethod phoneNumber:phoneNumberStr zone:zoneStr customIdentifier:nil result:^(NSError *error) {
+                
+                NSMutableDictionary *resultDic = [NSMutableDictionary dictionaryWithCapacity:0];
+                [resultDic setObject:[NSNumber numberWithInt:1] forKey:@"action"];
+                
+                if (!error )
+                {
+                    NSString *resultMsg = @"getCodeSuccess";
+                    [resultDic setObject:resultMsg forKey:@"res"];
+                    [resultDic setObject:[NSNumber numberWithInt:1] forKey:@"status"];
+                    //转化回到JSONString的状
+                    if (smsGetCodeMethod == SMSGetCodeMethodSMS)
                     {
-                        NSString *resultMsg = @"getCodeSuccess";
-                        [resultDic setObject:resultMsg forKey:@"res"];
-                        [resultDic setObject:[NSNumber numberWithInt:1] forKey:@"status"];
-                        //转化回到JSONString的状
-                        if (smsGetCodeMethod == SMSGetCodeMethodSMS)
-                        {
-                            NSLog(@"获取文本验证码成功后返回数据_%s",[resultMsg UTF8String]);
-                        }
-                        else
-                        {
-                           NSLog(@"获取语音验证码成功后返回数据_%s",[resultMsg UTF8String]);
-                        }
-                        
-                        NSString *resultStr = [MOBFJson jsonStringFromObject:resultDic];
-                        UnitySendMessage([observerStr UTF8String], "_callBack", [resultStr UTF8String]);
-                        
+                        NSLog(@"获取文本验证码成功后返回数据_%s",[resultMsg UTF8String]);
                     }
                     else
                     {
-                        NSMutableDictionary * resultErrorMsg =[NSMutableDictionary dictionaryWithObjectsAndKeys:@(error.code),@"code" ,error.domain,@"domain",error.userInfo,@"userInfo",  nil];
-                        //转化回到JSONString的状态码
-                        [resultDic setObject:[NSNumber numberWithInt:2] forKey:@"status"];
-                        NSString *resultMsg= [MOBFJson jsonStringFromObject:resultErrorMsg];
-                        [resultDic setObject:resultMsg forKey:@"res"];
-                        NSString *resultStr = [MOBFJson jsonStringFromObject:resultDic];
-                        
-                        // 参数1: unity中的场景对象名称,
-                        //参数2: unity中场景对象要执行的方法
-                        //参数3: ios向unity传递的参数
-                        UnitySendMessage([observerStr UTF8String], "_callBack", [resultStr UTF8String]);
+                        NSLog(@"获取语音验证码成功后返回数据_%s",[resultMsg UTF8String]);
                     }
                     
-                }];
-            }
-    
+                    NSString *resultStr = [MOBFJson jsonStringFromObject:resultDic];
+                    UnitySendMessage([observerStr UTF8String], "_callBack", [resultStr UTF8String]);
+                    
+                }
+                else
+                {
+                    NSMutableDictionary * resultErrorMsg =[NSMutableDictionary dictionaryWithObjectsAndKeys:@(error.code),@"code" ,error.domain,@"domain",error.userInfo,@"userInfo",  nil];
+                    //转化回到JSONString的状态码
+                    [resultDic setObject:[NSNumber numberWithInt:2] forKey:@"status"];
+                    NSString *resultMsg= [MOBFJson jsonStringFromObject:resultErrorMsg];
+                    [resultDic setObject:resultMsg forKey:@"res"];
+                    NSString *resultStr = [MOBFJson jsonStringFromObject:resultDic];
+                    
+                    // 参数1: unity中的场景对象名称,
+                    //参数2: unity中场景对象要执行的方法
+                    //参数3: ios向unity传递的参数
+                    UnitySendMessage([observerStr UTF8String], "_callBack", [resultStr UTF8String]);
+                }
+                
+            }];
         }
+        
+    }
     
     void __iosCommitVerificationCode (void *phoneNumber, void *zone, void *verificationCode,void *observer)
     {
@@ -129,7 +129,7 @@ extern "C" {
             verificationCodeStr = [NSString stringWithCString:verificationCode encoding:NSUTF8StringEncoding];
             observerStr = [NSString stringWithCString:observer encoding:NSUTF8StringEncoding];
             
-            [SMSSDK commitVerificationCode:verificationCodeStr phoneNumber:phoneNumberStr zone:zoneStr result:^(NSError *error) {
+            [SMSSDK commitVerificationCode:verificationCodeStr phoneNumber:phoneNumberStr zone:zoneStr result:^(SMSSDKUserInfo *userInfo,NSError *error) {
                 
                 NSMutableDictionary *resultDic = [NSMutableDictionary dictionaryWithCapacity:0];
                 [resultDic setObject:[NSNumber numberWithInt:2] forKey:@"action"];
@@ -154,7 +154,7 @@ extern "C" {
                     NSString *resultStr = [MOBFJson jsonStringFromObject:resultDic];
                     
                     UnitySendMessage([observerStr UTF8String], "_callBack", [resultStr UTF8String]);
-                
+                    
                 }
             }];
         }
@@ -192,7 +192,7 @@ extern "C" {
                     
                 }
             }];
-
+            
         }
         
     }
@@ -235,12 +235,12 @@ extern "C" {
     
     void __iosSubmitUserInfo (void *userInfoString,void *observer)
     {
-    
+        
         if (observer && userInfoString)
         {
             NSString *observerStr =  [NSString stringWithCString:observer encoding:NSUTF8StringEncoding];
             NSString *userInfoStr = [NSString stringWithCString:userInfoString encoding:NSUTF8StringEncoding];
-           
+            
             NSDictionary *userInfoDic = [MOBFJson objectFromJSONString:userInfoStr];
             SMSSDKUserInfo *userInfo = [[SMSSDKUserInfo alloc] init];
             
@@ -257,7 +257,7 @@ extern "C" {
                 
             }
             
-           
+            
             [SMSSDK submitUserInfoHandler:userInfo result:^(NSError *error)
              {
                  NSMutableDictionary *resultDic = [NSMutableDictionary dictionaryWithCapacity:0];
@@ -301,7 +301,7 @@ extern "C" {
             NSString *resultString = [MOBFJson jsonStringFromObject:resultDic];
             UnitySendMessage([observerStr UTF8String], "_callBack", [resultString UTF8String]);
         }
-       
+        
     }
     
     void __iosEnableAppContractFriends (BOOL state)
@@ -321,7 +321,7 @@ extern "C" {
             
             [SMSSDKUI showVerificationCodeViewWithMetohd:smsGetCodeMethod result:^(enum SMSUIResponseState state, NSString *phoneNumber, NSString *zone, NSError *error) {
                 NSMutableDictionary *resultDic = [NSMutableDictionary dictionaryWithCapacity:0];
-               
+                
                 if (!error)
                 {
                     if (state == SMSUIResponseStateSuccess)
@@ -355,7 +355,7 @@ extern "C" {
                 }
                 else
                 {
-                
+                    
                     NSMutableDictionary * resultErrorMsg =[NSMutableDictionary dictionaryWithObjectsAndKeys:@(error.code),@"code" ,error.domain,@"domain",error.userInfo,@"userInfo",  nil];
                     NSString *resultString = [MOBFJson jsonStringFromObject:resultErrorMsg];
                     [resultDic setObject:[NSNumber numberWithInt:2] forKey:@"status"];
@@ -363,13 +363,13 @@ extern "C" {
                     //转化回到JSONString的状态码
                     NSString *resultStr = [MOBFJson jsonStringFromObject:resultDic];
                     UnitySendMessage([observerStr UTF8String], "_callBack", [resultStr UTF8String]);
-                
+                    
                 }
                 
             }];
             
         }
-    
+        
     }
     
     void __showContractFriendsView (void *observer)
@@ -395,7 +395,7 @@ extern "C" {
             }];
             
         }
-    
+        
     }
     
     @implementation SMSSDK_Unity3dBridge
